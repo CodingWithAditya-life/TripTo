dc48709c8021eeed7920902d6f85367049e089a2
Aditya kumar <aditya@gmail.com>
2025-02-13 10:27:00 +0530

    added a new file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tripto/features/home/home_screen.dart';
+import 'package:tripto/firebase_options.dart';
 
-void main() {
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
