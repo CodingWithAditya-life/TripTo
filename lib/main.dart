d55df12f40c3b6c1814de3bc960db20547a3cb8d
Chandan123kumar <155878656+Chandan123kumar@users.noreply.github.com>
2025-02-13 11:38:38 +0530

    taking pull in master

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tripto/features/home/home_screen.dart';
 
-void main() {
+void main() async{
   runApp(const MyApp());
 }
 
@@ -11,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      home: Scaffold(),
     );
   }
 }
