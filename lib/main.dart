bd943cf634bcd34f6df6f7ad6950307ff65ce029
Aditya kumar <aditya@gmail.com>
2025-02-13 11:02:57 +0530

    Added a New Files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:tripto/features/authentication/home/home_page.dart';
 import 'package:tripto/firebase_options.dart';
 
 void main() async{
@@ -16,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: Scaffold(),
     );
   }
 }
